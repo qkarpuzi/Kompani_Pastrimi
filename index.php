64b8f32d7af8a87d4de6a64e1a89de238071b541
qkarpuzi <150028190+qkarpuzi@users.noreply.github.com>
2025-02-20 22:20:06 +0100

    test2

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Hello, World!</h1>";
+    ?>
+</body>
+</html>
